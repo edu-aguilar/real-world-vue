0860965b1240337ae8d47532cfb5145ab1474097
Edu Aguilar <eduardoaguilartirado@gmail.com>
2019-10-26 19:40:01 +0200

    feat(eventCard): created eventCard component and using it from home

---
+++ src/components/eventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <span>event-card component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "eventCard"
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  border: solid 1px red;
+  height: 100px;
+  width: 250px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="view home-view">
     <h1>This is the Home page</h1>
+
     <input type="text" v-model="id" />
     <router-link :to="{ name: routes.event.name, params: { id } }">
       {{ routes.event.label }} #{{ id }}
     </router-link>
+
+    <section class="card-list">
+      <eventCard />
+    </section>
   </div>
 </template>
 
 <script>
 import { DYNAMIC_ROUTES } from "./../router";
+import eventCard from "./../components/eventCard";
 
 export default {
   name: "Home",
+  components: {
+    eventCard
+  },
   data: function() {
     return {
       routes: DYNAMIC_ROUTES,
@@ -21,3 +30,12 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.card-list {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+</style>
