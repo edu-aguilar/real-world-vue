de224a6fdd4773859e91c1325382b643547bfa8b
Edu Aguilar <eduardoaguilartirado@gmail.com>
2019-10-27 09:04:30 +0100

    feat(api): created apiService to make http requests through axios

---
+++ src/services/apiService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const _axios = axios.create({
+  baseURL: "http://my-json-server.typicode.com/Code-Pop/real-world-vue",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-type": "application/json"
+  }
+});
+
+const ENDPOINTS = {
+  EVENTS: "/events"
+};
+
+const getEvents = () => {
+  return _axios.get(ENDPOINTS.EVENTS);
+};
+
+const getEvent = (eventId = 1) => {
+  return _axios.get(`${ENDPOINTS.EVENTS}/${eventId}`);
+};
+
+export { getEvents, getEvent };
